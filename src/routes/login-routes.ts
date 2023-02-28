b55e80aa791e9939e39ccb1b01ff2c8d7584398b
“Your <you@example.com>
2023-02-28 15:36:37 +0700

    done JP_21

--- src/routes/login-routes.ts
+++ src/routes/login-routes.ts
@@ -1,4 +1,5 @@
 import LoginPage from "@/pages/LoginPage";
+import MissPassLayout from "@/pages/LoginPage/components/MissPassLayout";
 import IRoute from "@/utils/helpers/route.helper";
 
 const routes: IRoute[] = [
@@ -14,6 +15,12 @@ const routes: IRoute[] = [
     name: "login",
     component: LoginPage,
   },
+  {
+    path: "/miss-pass",
+    key: "miss-pass",
+    name: "MissPass",
+    component: MissPassLayout
+  }
 ];
 
 export default routes;
